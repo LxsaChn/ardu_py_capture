c2debe72a7016d9d057a6c79091c3d3ebd565be3
LxsaChn <62408538+LxsaChn@users.noreply.github.com>
2023-04-04 01:08:51 -0700

    Update ArduCAM_Mini_5MP_Plus_VideoStreaming.py

--- Python/ArduCAM_Mini_5MP_Plus_VideoStreaming.py
+++ Python/ArduCAM_Mini_5MP_Plus_VideoStreaming.py
@@ -18,11 +18,13 @@ buffer=bytearray(once_number)
 mycam = ArducamClass(OV5642)
 mycam.Camera_Detection()
 mycam.Spi_Test()
+mycam.set_format(JPEG)
 mycam.Camera_Init()
 mycam.Spi_write(ARDUCHIP_TIM,VSYNC_LEVEL_MASK)
 utime.sleep(1)
 mycam.clear_fifo_flag()
 mycam.Spi_write(ARDUCHIP_FRAMES,0x00)
+
 
 def read_fifo_burst():
     count=0
